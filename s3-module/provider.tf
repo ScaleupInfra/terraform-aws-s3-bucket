ce90849ce6500de67c925b7f69fbd150a4a753ec
tusharkumar <tusharkumar27864@gmail.com>
2023-06-27 14:25:56 +0530

    creating an s3 bucket

--- s3-module/provider.tf
+++ s3-module/provider.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 terraform {
-  required_version = ">= 1.0.0"  # Specify the desired Terraform version here
+  # required_version = ">= 1.0.0"  # Specify the desired Terraform version here
   required_providers {
     aws = ">= 3.0.0"  # Specify the desired AWS provider version here
   }
