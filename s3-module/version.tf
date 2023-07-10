90777a82eb29674c0bb3c38729f0b30b3d8ae8ca
tusharkumar <tusharkumar27864@gmail.com>
2023-07-10 18:50:03 +0530

    made changes in the tushar branch

--- s3-module/version.tf
+++ s3-module/version.tf
@@ -1,6 +1,6 @@
 terraform {
-  # required_version = ">= 1.0.0"  # Choose the desired Terraform version here
+  # required_version = ">= 1.0.0"  # select the desired Terraform version here
   required_providers {
-    aws = ">= 3.0.0" # Choose the desired AWS provider version here
+    aws = ">= 3.0.0" # select the desired AWS provider version here
   }
 }
